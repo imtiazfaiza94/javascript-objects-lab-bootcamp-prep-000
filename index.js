dd6614b6cc7654342a25a415261b41e9eb059a18
Faiza Imtiaz <imtiazfaiza94@gmail.com>
2019-02-13 00:09:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-var recipes = {
-  cake: 'sugar, butter, eggs, vanilla extract, flour, baking powder, milk',
-  smoothie: 'fruit, milk, ice'
-} // recipes
- function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
-} // updateObjectWithKeyAndValue
- function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
- object[key] = value
- return object
-} // destructivelyUpdateObjectWithKeyAndValue
- function deleteFromObjectByKey(object, key) {
-  var newRecipe = Object.assign({}, object)
-  newRecipe // { foo: 'bar' }
-  delete newRecipe.key // true
-  return newRecipe
-} // deleteFromObjectByKey
- function destructivelyDeleteFromObjectByKey(object, key) {
+var recipes = new Object ({})
+
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
   return object
-} // destructivelyUpdateObjectWithKeyAndValue
+}
